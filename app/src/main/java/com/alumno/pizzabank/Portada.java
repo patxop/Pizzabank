c6bf7a7a2e51d2dfa8161e9e2b820359f58b0c10
patxop <patxopcebanc@gmail.com>
2016-12-09 14:21:20 +0100

    Initial commit

---
+++ app/src/main/java/com/alumno/pizzabank/Portada.java
@@ -0,0 +1,13 @@
+package com.alumno.pizzabank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Portada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+    }
+}
